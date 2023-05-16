082e9975ffefaba36e0881849d715cf41d8a316f
Suyoooi <131094271+Suyoooi@users.noreply.github.com>
2023-05-16 11:47:50 +0900

    feat: setting

---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import { Inter } from "next/font/google";
+import Header from "@/layout/Header";
+import Sidebar from "@/layout/Sidebar";
+import Layout from "@/layout/layout";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <>
+      <Layout />
+      <Header />
+      <Sidebar />
+    </>
+  );
+}
