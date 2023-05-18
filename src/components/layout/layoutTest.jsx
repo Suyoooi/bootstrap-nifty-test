58e1348a63c814c99bca481b5bc52a4deb72619e
Suyoooi <131094271+Suyoooi@users.noreply.github.com>
2023-05-18 09:23:26 +0900

    feat: layout 구현

--- src/components/layout/layoutTest.jsx
+++ src/components/layout/layoutTest.jsx
@@ -1,22 +1,13 @@
-import SideMenu from "@/components/sidebar";
-import Header from "@/components/header";
 import BHeader from "../Bheader";
 import BSidebar from "../Bsidebar";
 
-export default function Layout({ children }) {
+export default function LayoutTest({ children }) {
   return (
     <>
-      {/* <Header />
-      <SideMenu /> */}
-      {/* <BHeader /> */}
-      {/* 여기서 children은 layout 컴포넌트가 감싸고 있는 다른 컴포넌트를 의미한다. */}
-      {/* <main>{children}</main> */}
-
-      {/*PAGE CONTAINER */}
       <div id="root" className="root mn--max hd--expanded">
         {/* content */}
         <section id="content" className="content">
-          {props.children}
+          {children}
         </section>
         <BHeader />
         <BSidebar />
